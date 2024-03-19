4698630e6369d8878c76cf28fe5271cc0167f1fe
Ashish Singh <118279121+kaptaan14@users.noreply.github.com>
2024-03-19 13:29:55 +0530

    fixed

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,60 +1,59 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
-import { getSocketReceiverId } from "../socket/socket.js";
+import { getSocketReceiverId,io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-		const { id: receiverId } = req.params;
-		const senderId = req.user._id;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-		let conversation = await Conversation.findOne({
-			participants: { $all: [senderId, receiverId] },
-		});
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-		if (!conversation) {
-			conversation = await Conversation.create({
-				participants: [senderId, receiverId],
-			});
-		}
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
 
-		const newMessage = new Message({
-			senderId,
-			receiverId,
-			message,
-		});
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
 
-		if (newMessage) {
-			conversation.messages.push(newMessage._id);
-		}
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
 
-		await Promise.all([conversation.save(), newMessage.save()]);
+    await Promise.all([conversation.save(), newMessage.save()]);
 
-		const receiverSocketId = getSocketReceiverId(receiverId);
-		if (receiverSocketId) {
-			io.to(receiverSocketId).emit("newMessage", newMessage);
-		}
+    const receiverSocketId = getSocketReceiverId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
-		res.status(201).json(newMessage);
+    res.status(201).json(newMessage);
   } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error 67" });
+    return res.status(500).json({ error: "Internal Server Error 68" });
   }
 };
 
 export const getMessages = async (req, res) => {
   try {
-    const { id } = req.params;
+    const { id: userToChatId } = req.params;
     const senderId = req.user._id;
 
     const conversation = await Conversation.findOne({
-      participants: { $all: [senderId, id] },
-    }).populate("messages");
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); 
 
-    if (!conversation) {
-      return res.status(201).json([]);
-    }
+    if (!conversation) return res.status(200).json([]);
 
     const messages = conversation.messages;
+
     res.status(200).json(messages);
   } catch (error) {
     console.log("Error in getting message");
