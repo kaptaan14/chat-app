cffe83aae4dedb841f64d537e6b59c3f383150ec
Ashish Singh <118279121+kaptaan14@users.noreply.github.com>
2024-03-19 12:19:15 +0530

    fixed

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -5,37 +5,37 @@ import { getSocketReceiverId } from "../socket/socket.js";
 export const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
-    const { id: receiverId } = req.params;
-    const senderId = req.user._id;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
 
-    let conversation = await Conversation.findOne({
-      participants: { $all: [senderId, receiverId] },
-    });
+		let conversation = await Conversation.findOne({
+			participants: { $all: [senderId, receiverId] },
+		});
 
-    if (!conversation) {
-      conversation = await Conversation.create({
-        participants: [senderId, receiverId],
-      });
-    }
+		if (!conversation) {
+			conversation = await Conversation.create({
+				participants: [senderId, receiverId],
+			});
+		}
 
-    const newMessage = new Message({
-      senderId,
-      receiverId,
-      message,
-    });
+		const newMessage = new Message({
+			senderId,
+			receiverId,
+			message,
+		});
 
-    if (newMessage) {
-      conversation.messages.push(newMessage._id);
-    }
+		if (newMessage) {
+			conversation.messages.push(newMessage._id);
+		}
 
-    await Promise.all([conversation.save(), newMessage.save()]);
+		await Promise.all([conversation.save(), newMessage.save()]);
 
-    const receiverSocketId = getSocketReceiverId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
+		const receiverSocketId = getSocketReceiverId(receiverId);
+		if (receiverSocketId) {
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
-    res.status(201).json(newMessage);
+		res.status(201).json(newMessage);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error 67" });
   }
